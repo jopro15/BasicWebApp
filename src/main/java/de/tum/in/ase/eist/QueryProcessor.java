6c8b24885bacaf1eae5ef3efa726c6f790fac588
Jonas Seidou <jomokit@gmail.com>
2022-07-14 18:50:49 +0200

    h

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,9 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
-            return "";
+        } else {
+
+            return "I don't know shit about " + query + ", so stop asking me these questions!";
         }
     }
 }
